ea37e8a341a891b9188de22ceb5a8d159fb3a1ed
duylvs <duyluu51@gmail.com>
2023-06-10 07:11:12 +0700

    Updates

---
+++ precache-manifest.208855aea084e623f05f1249b6086660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71518888e6dff9fa5a859724bbceade",
+    "url": "/cv-luuVuSonDuy/index.html"
+  },
+  {
+    "revision": "cb3ea949e81996140a36",
+    "url": "/cv-luuVuSonDuy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36a09510461e3833e7e1",
+    "url": "/cv-luuVuSonDuy/static/js/2.d88353d6.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/cv-luuVuSonDuy/static/js/2.d88353d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3ea949e81996140a36",
+    "url": "/cv-luuVuSonDuy/static/js/main.de8d9f03.chunk.js"
+  },
+  {
+    "revision": "89e8af1022f14bb19b66",
+    "url": "/cv-luuVuSonDuy/static/js/runtime-main.bd704d9f.js"
+  }
+]);
